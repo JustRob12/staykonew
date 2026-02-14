0e092960b50ff27b7f1cd4b36726d14f8c4b4696
MeDevRob <118245001+JustRob12@users.noreply.github.com>
2026-02-14 19:06:05 +0800

    fixed

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { RouteLoadingBar } from "@/components/RouteLoadingBar";
+import { Suspense } from "react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,7 +35,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         suppressHydrationWarning
       >
-        <RouteLoadingBar />
+        <Suspense fallback={null}>
+          <RouteLoadingBar />
+        </Suspense>
         {children}
       </body>
     </html>
